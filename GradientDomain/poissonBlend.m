647971ecc9104e6524dd07482c191dfd634cf50e
Robert Miller <millerrt3@gmail.com>
2016-02-20 19:30:07 -0600

    Progress on Poisson blending

--- GradientDomain/poissonBlend.m
+++ GradientDomain/poissonBlend.m
@@ -1,62 +1,89 @@
-function [ blended_img ] = poissonBlend(fg_img, mask, bg_img)
+function [ blended_img ] = poissonBlend(object, mask, target)
 %poissonBlend Blend two images using the provided mask
-%   TODO
+%   object: the image fragment to be blended in
+%   mask:   the logical mask of where in the target image the object should appear
+%   target: the image into which the object will be blended
 
     % Obtain the indices of the source image and initialize a vector of indices into the image
-    [height, width, channels] = size(bg_img);
+    [height, width, channels] = size(target);
     pixelIndices = zeros(height, width); 
     pixelIndices(1:height*width) = 1:height*width;
     
-    blended_img = zeros(size(bg_img));
+    blended_img = zeros(size(target));
     
     for c = 1:channels
         % Initalize the equation counter to 0
-        e = 1;
+        e = 0;
         
         % Initalize the constraint matrices (using sparse matrices since the vast majority of values
         %   in the matrices will be zeros)
-        A = sparse(height * width * 8, height * width);
-        b = sparse(height * width * 8, 1);
+        A = sparse(height * width * 4, height * width);
+        b = sparse(height * width * 4, 1);
         
         % Set up the constraints to be solved
         for x = 2:width-1
             for y = 2:height-1
-                % Constraint 1: Similarity with source
-                A(e, pixelIndices(y,x)) = 1;
-                A(e, pixelIndices(y,x-1)) = -mask(y,x-1);
-                b(e) = bg_img(y,x,c) - bg_img(y,x-1,c);
-                
-                A(e+1, pixelIndices(y,x)) = 1;
-                A(e+1, pixelIndices(y,x+1)) = -mask(y,x+1);
-                b(e+1) = bg_img(y,x,c) - bg_img(y,x+1,c);
-                
-                A(e+2, pixelIndices(y,x)) = 1;
-                A(e+2, pixelIndices(y-1,x)) = -mask(y-1,x);
-                b(e+2) = bg_img(y,x,c) - bg_img(y-1,x,c);
-                
-                A(e+3, pixelIndices(y,x)) = 1;
-                A(e+3, pixelIndices(y+1,x)) = -mask(y+1,x);
-                b(e+3) = bg_img(y,x,c) - bg_img(y+1,x,c);
-                
-                % Constraint 2: Similarity with target
-                A(e+4, pixelIndices(y,x)) = 1;
-                b(e+4) = bg_img(y,x,c) - bg_img(y,x-1,c) + mask(y,x)*fg_img(y,x-1,c);
-                
-                A(e+5, pixelIndices(y,x)) = 1;
-                b(e+5) = bg_img(y,x,c) - bg_img(y,x+1,c) + mask(y,x)*fg_img(y,x+1,c);
-                
-                A(e+6, pixelIndices(y,x)) = 1;
-                b(e+6) = bg_img(y,x,c) - bg_img(y-1,x,c) + mask(y,x)*fg_img(y-1,x,c);
-                
-                A(e+7, pixelIndices(y,x)) = 1;
-                b(e+7) = bg_img(y,x,c) - bg_img(y+1,x,c) + mask(y,x)*fg_img(y+1,x,c);
+                % If within the confines of the mask, apply the blending constraints
+                if mask(y,x)
+                    % Left neighbor
+                    e = e + 1;
+                    if ~mask(y,x-1)
+                        A(e, pixelIndices(y,x)) = 1;
+                        b(e) = target(y,x,c) - target(y,x-1,c) + object(y,x-1,c);   
+                    else
+                        A(e, pixelIndices(y,x)) = 1;
+                        A(e, pixelIndices(y,x-1)) = -1;
+                        b(e) = target(y,x,c) - target(y,x-1,c);
+                    end
+                    
+                    % Right neighbor
+                    e = e + 1;
+                    if ~mask(y,x+1)
+                        A(e, pixelIndices(y,x)) = 1;
+                        b(e) = target(y,x,c) - target(y,x+1,c) + object(y,x+1,c);
+                    else
+                        A(e, pixelIndices(y,x)) = 1;
+                        A(e, pixelIndices(y,x+1)) = -1;
+                        b(e) = target(y,x,c) - target(y,x+1,c);
+                    end
+                    
+                    % Top neighbor
+                    e = e + 1;
+                    if ~mask(y-1,x)
+                        A(e, pixelIndices(y,x)) = 1;
+                        b(e) = target(y,x,c) - target(y-1,x,c) + object(y-1,x,c);   
+                    else
+                        A(e, pixelIndices(y,x)) = 1;
+                        A(e, pixelIndices(y-1,x)) = -1;
+                        b(e) = target(y,x,c) - target(y-1,x,c);
+                    end
+                    
+                    % Bottom neighbor
+                    e = e + 1;
+                    if ~mask(y+1,x)
+                        A(e, pixelIndices(y,x)) = 1;
+                        b(e) = target(y,x,c) - target(y+1,x,c) + object(y+1,x,c);   
+                    else
+                        A(e, pixelIndices(y,x)) = 1;
+                        A(e, pixelIndices(y+1,x)) = -1;
+                        b(e) = target(y,x,c) - target(y+1,x,c);
+                    end
+                end
             end
-            e = e + 8;
         end
         
         % Use the constraints above to solve for the blended image, reshaping it to match the
         %   dimensions of the source image
         blended_img(:,:,c) = full(reshape(A \ b, [height, width]));
+    end
+    
+    for x = 1:width
+        for y = 1:height
+            if ~mask(y,x)
+                % Copy over pixels from the original image outside of the blended area
+                blended_img(y,x,:) = target(y,x,:);
+            end
+        end
     end
 end
 
--- GradientDomain/toy_reconstruct.m
+++ GradientDomain/toy_reconstruct.m
@@ -1,12 +1,10 @@
 function [reconstructed] = toy_reconstruct(image)
 %toy_reconstruct Reconstruct an image from its gradient
-%   After converting the original image (provided as an argument) into the
-%     gradient domain, this function reconstructs the image (using a single
-%     pixel as a point of reference) and returns it to the function's
-%     caller
+%   After converting the original image (provided as an argument) into the gradient domain, this
+%     function reconstructs the image (using a single pixel as a point of reference) and returns it 
+%     to the function's caller
 
-    % Obtain the indices of the source image and initialize a vector of
-    %   indices into the image
+    % Obtain the indices of the source image and initialize a vector of indices into the image
     [height, width, channels] = size(image);
     pixelIndices = zeros(height, width); 
     pixelIndices(1:height*width) = 1:height*width;
@@ -17,13 +15,12 @@ function [reconstructed] = toy_reconstruct(image)
         % Initialize the equation counter to 0
         e = 0;
 
-        % Initalize the constraint matrices (using sparse matrices since
-        %   the vast majority of values in the matrices will be zeros)
+        % Initalize the constraint matrices (using sparse matrices since the vast majority of values
+        %   in the matrices will be zeros)
         A = sparse(height * width * 2 + 1, height * width);
         b = zeros(height * width * 2 + 1, 1);
 
-        % Set up the first constraint: equivalence of the horizontal
-        %   components of the gradients
+        % Set up the first constraint: equivalence of the horizontal components of the gradients
         for x = 1:width-1
             for y = 1:height
                 e = e + 1;
@@ -33,8 +30,7 @@ function [reconstructed] = toy_reconstruct(image)
             end
         end
 
-        % Set up the second constraint: equivalence of the vertical
-        %   components of the gradients
+        % Set up the second constraint: equivalence of the vertical components of the gradients
         for x = 1:width
             for y = 1:height-1
                 e = e + 1;
@@ -44,15 +40,14 @@ function [reconstructed] = toy_reconstruct(image)
             end
         end
 
-        % Set up the third constraint: the top left pixel of the
-        %   reconstructed image should have the same intensity as the pixel
-        %   at the same location in the source image
+        % Set up the third constraint: the top left pixel of the reconstructed image should have the
+        %   same intensity as the pixel at the same location in the source image
         e = e + 1;
         A(e, pixelIndices(1,1)) = 1;
         b(e) = image(1,1,c);
 
-        % Use the constraints above to solve for the reconstructed image,
-        %   reshaping it to match the dimensions of the source image
+        % Use the constraints above to solve for the reconstructed image, reshaping it to match the
+        %   dimensions of the source image
         reconstructed(:,:,c) = full(reshape(A \ b, [height, width]));
     end
 end
