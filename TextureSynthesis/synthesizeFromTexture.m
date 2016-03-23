f0c4300f3cad14bd3db59810a66841529b9e92c2
Robert Miller <millerrt3@gmail.com>
2016-03-22 20:36:02 -0500

    First draft of synthesis w/o graph cut

--- TextureSynthesis/synthesizeFromTexture.m
+++ TextureSynthesis/synthesizeFromTexture.m
@@ -1,17 +1,55 @@
-function synthesized = synthesizeFromTexture(srcTexture, numPatches, overlap, synthDims)
+function synthesized = synthesizeFromTexture(srcTexture, patchDims, overlap, synthDims)
 %synthesizeFromTexture Generate a new image with the same texture as some source image
 %   Based on the source image in srcTexture, this function will generate a new image with the same
 %     texture.
 %
 %   srcTexture: the image containing the texture to replicate
-%   numPatches: the number of patches to be used for generation (in the form of a 2-element vector)
+%   patchDims: the desired dimensions for patch sampling (as a 2D vector)
 %   overlap:    by how many pixels the patches should overlap (another 2D vector)
 %   synthDims:  the desired size of the output image
 
-    synthesized = srcTexture;
+    synthesized = zeros(synthDims);
     
-    % Copy a random patch into the top left corner of the image
-    % For every other patch in the image, compute the SSD of the overlap with its neighbors
-    % Use Poisson blending to insert the new patch into the correct poisition
-
+    for i = 1:patchDims(2) - overlap(2):synthDims(2)
+        for j = 1:patchDims(1) - overlap(1):synthDims(1)
+            
+            % Compute patch boundaries
+            xBound = i + patchDims(2) - 1;
+            if xBound > synthDims(2)
+                xBound = synthDims(2);
+            end
+            yBound = j + patchDims(1) - 1;
+            if yBound > synthDims(1)
+                yBound = synthDims(1);
+            end
+            
+            % Find the target region in the synthesized image
+            targetPatch = synthesized(j:yBound, i:xBound, :);
+            
+            % Find the minimum-error patch
+            bestErrSurface = ones(patchDims);
+            bestErr = Inf;
+            bestPatch = [0, 0];
+            for ii = 1:size(targetPatch,2):size(srcTexture, 2) - size(targetPatch,2)
+                for jj = 1:size(targetPatch,1):size(srcTexture, 1) - size(targetPatch,1)
+                    testPatch = srcTexture(jj:jj + size(targetPatch,1) - 1, ...
+                        ii:ii + size(targetPatch,2) - 1, :);
+                    testPatch = testPatch.*(targetPatch ~= 0);
+                    testErrSurface = (testPatch - targetPatch).^2;
+                    testErr = sum(testErrSurface(:));
+                    
+                    if testErr < bestErr
+                        bestErr = testErr;
+                        bestErrSurface = testErrSurface;
+                        bestPatch = [ii, jj];
+                    end
+                end
+            end
+            
+            % Insert the best-matching patch into the generated image
+            synthesized(j:j + size(targetPatch,1) - 1, i:i + size(targetPatch,2) - 1, :) = ...
+                srcTexture(bestPatch(2):bestPatch(2) + size(targetPatch,1) - 1, ...
+                    bestPatch(1):bestPatch(1) + size(targetPatch,2) - 1, :);
+        end
+    end
 end
